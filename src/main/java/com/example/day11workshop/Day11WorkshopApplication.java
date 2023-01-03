83d40111882038b4caf9f255208d96d6cf5984fe
Ethanleewc <ethanleewc@gmail.com>
2023-01-03 13:57:58 +0800

    first commit for day11-workshop

---
+++ src/main/java/com/example/day11workshop/Day11WorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.day11workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/day11workshop/Day11WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day11workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
